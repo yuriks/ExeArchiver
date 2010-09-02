3a1079eddada5cd44607c8e95bcff0a56bd3d349
Yuri Kunde Schlesner <yuriks.br@gmail.com>
2010-09-02 18:19:27 -0300

    Wrote exear

--- ExeAr/src/main.cpp
+++ ExeAr/src/main.cpp
@@ -16,6 +16,134 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <cstdint>
+
+uint32_t get_file_size(std::ifstream& f)
+{
+	std::ifstream::pos_type old_pos = f.tellg();
+
+	f.seekg(0, std::ios::end);
+	uint32_t size = static_cast<uint32_t>(f.tellg());
+
+	f.seekg(old_pos);
+
+	std::cerr << "Tamanho: " << size << std::endl;
+
+	return size;
+}
+
+std::ostream& write_u16(std::ostream& f, uint32_t val)
+{
+	f.put((val >> 8)  & 0xFF);
+	f.put((val >> 0)  & 0xFF);
+
+	return f;
+}
+
+std::ostream& write_u32(std::ostream& f, uint32_t val)
+{
+	f.put((val >> 24) & 0xFF);
+	f.put((val >> 16) & 0xFF);
+	f.put((val >> 8)  & 0xFF);
+	f.put((val >> 0)  & 0xFF);
+
+	return f;
+}
+
+std::ostream& copy_file_contents(std::istream& infile, std::ostream& outfile)
+{
+	static const int buf_size = 16 * 1024;
+	char buf[buf_size];
+
+	infile.seekg(0);
+
+	do
+	{
+		infile.read(buf, buf_size);
+		outfile.write(buf, infile.gcount());
+	} while (infile.gcount() > 0);
+
+	return outfile;
+}
+
 int main(int argc, char* argv[])
 {
+	if (argc != 3)
+	{
+		std::cerr << "Usage:\n  " << argv[0] << " outfile filelist" << std::endl;
+		return 1;
+	}
+
+	std::ifstream filelist_file(argv[2]);
+	if (!filelist_file)
+	{
+		std::cerr << "Error opening " << argv[2] << std::endl;
+		return 2;
+	}
+
+	std::ofstream outfile(argv[1], std::ios::out | std::ios::binary);
+	if (!outfile)
+	{
+		std::cerr << "Error opening " << argv[1] << std::endl;
+		return 2;
+	}
+
+	// nome / offset
+	typedef std::vector<std::pair<std::string, std::uint32_t>> FileList;
+	FileList file_list;
+
+	uint32_t cur_offset = 0;
+
+	std::string fname;
+	while (std::getline(filelist_file, fname))
+	{
+		if (fname.empty())
+			continue;
+
+		std::ifstream infile(fname, std::ios::in | std::ios::binary);
+		if (!infile)
+		{
+			std::cerr << "Error opening " << fname << " - Skipped" << std::endl;
+			continue;
+		}
+
+		uint32_t size = get_file_size(infile);
+
+		std::cerr << "Processing \"" << fname << "\" Size: " << size << std::endl;
+
+		write_u32(outfile, size);
+		copy_file_contents(infile, outfile);
+
+		file_list.push_back(make_pair(fname, cur_offset));
+		cur_offset += size + 4; // +4 is uint32_t size
+	}
+
+	filelist_file.close();
+
+	write_u32(outfile, cur_offset); // Contents size
+	cur_offset = 0;
+
+	for (FileList::const_iterator i = file_list.begin(); i != file_list.end(); ++i)
+	{
+		write_u16(outfile, i->first.length());
+		outfile << i->first;
+		write_u32(outfile, i->second);
+
+		cur_offset += 2 + i->first.length() + 4;
+	}
+
+	write_u16(outfile, 0xFFFF); // End of file list
+
+	write_u32(outfile, cur_offset); // File list size
+
+	write_u16(outfile, 0); // Version
+	write_u16(outfile, 0x0001); // Endianess
+	outfile << "EXEARCHIVE";
+
+	return 0;
 }
